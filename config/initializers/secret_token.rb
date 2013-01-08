1afca590cd7aee265c706fb7485e451f37e41d88
Gab Ub <gab@gubu6.hu>
2013-01-08 21:58:40 +0100

    Initial commit - rails base
    + .gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'fd4854b26570d1587af9af8cda2db7406a3561a1c91b9a98f56009a32868a56af1d31896405baba0578eadf6535519c4db0529d47f33ab87d9c9694704dcacea'
